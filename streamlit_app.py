43fa8cf1b951fae30d9acfb3bc1296b99df83a4a
almight1213 <126928845+almight1213@users.noreply.github.com>
2026-07-23 23:20:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -848,11 +848,11 @@ if launch:
     suggestion = st.session_state.volume_optimization_suggestion
     if suggestion is not None:
             render_optimization_banner(suggestion)    
-        st.markdown("<div style='height: 0.85rem;'></div>", unsafe_allow_html=True)
-        st.markdown("<div class='results-title' style='font-size:0.95rem;'>EQUITY CURVE</div>", unsafe_allow_html=True)
-        st.line_chart(st.session_state.equity, use_container_width=True)
-        st.markdown("<div style='height: 1.0rem;'></div>", unsafe_allow_html=True)
-        st.markdown("<div class='results-title' style='font-size:0.95rem;'>Trade Log</div>", unsafe_allow_html=True)
+    st.markdown("<div style='height: 0.85rem;'></div>", unsafe_allow_html=True)
+    st.markdown("<div class='results-title' style='font-size:0.95rem;'>EQUITY CURVE</div>", unsafe_allow_html=True)
+    st.line_chart(st.session_state.equity, use_container_width=True)
+    st.markdown("<div style='height: 1.0rem;'></div>", unsafe_allow_html=True)
+    st.markdown("<div class='results-title' style='font-size:0.95rem;'>Trade Log</div>", unsafe_allow_html=True)
 
         trades_for_log = st.session_state.trade_log
         if not trades_for_log:
